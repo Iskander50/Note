68689eaf7f8d3e4ea812dbe824a62d518c289067
Iskander50 <143713443+Iskander50@users.noreply.github.com>
2023-12-06 15:33:54 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React,{Component} from 'react';
+import Form from '../src/components/Form'
+
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="container App">
+          <Form/>
+        </div>
+    );
+  }
+  
+}
+
+export default App;
